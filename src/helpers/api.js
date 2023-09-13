3f43286882c383d71f340fb8377fb9e2614b2a93
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-09-13 05:06:02 +0100

    Update api.js

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,13 +1,13 @@
-export const getMovies = async (setMoviesList) => {
-	const options = {
-		method: "GET",
-		headers: {
-			accept: process.env.REACT_APP_APPLICATION,
-			Authorization:
-				"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMGI2YjcwMjY1Mzk4ZjYwYWY1ZGUwYjIwZjAwZjBkYSIsInN1YiI6IjY0ZmY0YWQ5MmRmZmQ4MDExZGYwYTMwMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Bsdd9DjTcSJDQkf66d-y6gbYi6c2fdEZFQscQEVCBb4",
-		},
-	};
+export const options = {
+	method: "GET",
+	headers: {
+		accept: "application/json",
+		Authorization:
+			"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMGI2YjcwMjY1Mzk4ZjYwYWY1ZGUwYjIwZjAwZjBkYSIsInN1YiI6IjY0ZmY0YWQ5MmRmZmQ4MDExZGYwYTMwMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Bsdd9DjTcSJDQkf66d-y6gbYi6c2fdEZFQscQEVCBb4",
+	},
+};
 
+export const getMovies = async (setMoviesList) => {
 	const response = await fetch(
 		"https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
 		options
