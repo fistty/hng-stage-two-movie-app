f56b1f88fd5227c9275d8fd0f80bb0dd46256eb2
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-09-13 19:33:04 +0100

    new UI

---
+++ src/components/MovieBackdrop.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import playSvg from "../../assets/play.svg";
+
+export const MovieBackdrop = () => {
+	const base = "https://image.tmdb.org/t/p/w1280";
+
+	return (
+		<div className="movie-backdrop">
+			<img
+				src={` ${base}/${"/kXfqcdQKsToO0OUXHcrrNCHDBzO.jpg"}`}
+				alt="movie-poster"
+			/>
+			<div className="play-button">
+				<button>
+					<img src={playSvg} alt="" />
+				</button>
+			</div>
+		</div>
+	);
+};
--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -1,20 +1,42 @@
 import React from "react";
-import playSvg from "../../assets/play.svg";
+import { MovieBackdrop } from "./MovieBackdrop";
+import ticketSvg from "../../assets/two-tickets.svg";
+import listSvg from "../../assets/list.svg";
 import "./MovieInfo.css";
 
 export const MovieInfo = () => {
-	const base = "https://image.tmdb.org/t/p/w1280";
 	return (
-		<div classwork="movie-info">
-			<div className="movie-backdrop">
-				<img
-					src={` ${base}/${"/kXfqcdQKsToO0OUXHcrrNCHDBzO.jpg"}`}
-					alt="movie-poster"
-				/>
-				<div className="play-button">
+		<div className="movie-info">
+			<MovieBackdrop />
+			<div className="movie-description">
+				<div className="movie-title">
+					<p>Top Gun: Maverick • 2022 • PG-13 • 2h 10m</p>
+				</div>
+				<div className="movie-text">
+					<p>
+						After thirty years, Maverick is still pushing the envelope as a top naval
+						aviator, but must confront ghosts of his past when he leads TOP GUN's
+						elite graduates on a mission that demands the ultimate sacrifice from
+						those chosen to fly it.
+					</p>
+				</div>
+			</div>
+			<div className="movie-watch">
+				<div className="movie-rating">
+					<p>8.5|350k</p>
+				</div>
+				<div className="movie-showtimes">
+					{" "}
 					<button>
-						<img src={playSvg} alt="" />
-					</button>
+						<img src={ticketSvg} alt="" />
+						See Showtime
+					</button>{" "}
+				</div>
+				<div className="movie-more">
+					<button>
+						<img src={listSvg} alt="" />
+						More watch options
+					</button>{" "}
 				</div>
 			</div>
 		</div>
