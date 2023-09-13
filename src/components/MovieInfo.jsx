c6f07d25ef97377c61e33291d1da6d5ca69b913d
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-09-13 21:21:50 +0100

    UI

--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { MovieBackdrop } from "./MovieBackdrop";
 import ticketSvg from "../../assets/two-tickets.svg";
 import listSvg from "../../assets/list.svg";
+import starSvg from "../../assets/star.svg";
 import "./MovieInfo.css";
 
 export const MovieInfo = () => {
@@ -9,34 +10,37 @@ export const MovieInfo = () => {
 		<div className="movie-info">
 			<MovieBackdrop />
 			<div className="movie-description">
-				<div className="movie-title">
-					<p>Top Gun: Maverick • 2022 • PG-13 • 2h 10m</p>
+				<div className="first-div">
+					<div className="movie-title">
+						<p>Top Gun: Maverick • 2022 • PG-13 • 2h 10m</p>
+					</div>
+					<div className="movie-text">
+						<p>
+							After thirty years, Maverick is still pushing the envelope as a top naval
+							aviator, but must confront ghosts of his past when he leads TOP GUN's
+							elite graduates on a mission that demands the ultimate sacrifice from
+							those chosen to fly it.
+						</p>
+					</div>
 				</div>
-				<div className="movie-text">
-					<p>
-						After thirty years, Maverick is still pushing the envelope as a top naval
-						aviator, but must confront ghosts of his past when he leads TOP GUN's
-						elite graduates on a mission that demands the ultimate sacrifice from
-						those chosen to fly it.
-					</p>
-				</div>
-			</div>
-			<div className="movie-watch">
-				<div className="movie-rating">
-					<p>8.5|350k</p>
-				</div>
-				<div className="movie-showtimes">
-					{" "}
-					<button>
-						<img src={ticketSvg} alt="" />
-						See Showtime
-					</button>{" "}
-				</div>
-				<div className="movie-more">
-					<button>
-						<img src={listSvg} alt="" />
-						More watch options
-					</button>{" "}
+				<div className="second-div">
+					<div className="movie-rating">
+						<img src={starSvg} alt="rating image" />
+						<p className="movie-rating-average">8.5</p>
+						<p className="movie-rating-count">|350k</p>
+					</div>
+					<div className="movie-showtimes ">
+						<button className="first second-div-button">
+							<img src={ticketSvg} alt="" />
+							See Showtime
+						</button>
+					</div>
+					<div className="movie-more ">
+						<button className="second second-div-button">
+							<img src={listSvg} alt="" />
+							More watch options
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
