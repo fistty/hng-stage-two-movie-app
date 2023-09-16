5af105404b8f57dc76e588d0ebeb76e44a646adc
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-09-16 04:03:50 +0100

    newCh

--- src/helpers/getImages.js
+++ src/helpers/getImages.js
@@ -1,6 +1,6 @@
 export const getImages = (imageUrl) => {
 	if (!imageUrl) {
-		return "../../assets/placeholder.svg";
+		return "../../assets2/placeholder.svg";
 	}
 	const baseImageUrl = "https://image.tmdb.org/t/p/w185";
 	return `${baseImageUrl}/${imageUrl}`;
