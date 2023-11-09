41a1dd9818e0a0dd6db96c8d4c1d47329a9ede78
Osuntuyi Ayomide Amos <osuntuyiemilos@gmail.com>
2023-11-09 16:24:46 +0100

    Update RootLayout.jsx

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import axios from "axios";
 import { Outlet } from "react-router-dom";
-import { useMovieContext } from "../context/useMovieContext";
-import { Overlay } from "../components/Overlay";
 
 function RootLayout() {
-	const { isOverlay } = useMovieContext();
-
 	return (
 		<>
 			<Outlet />
